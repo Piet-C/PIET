2c1f55e9dfc87fcaad695cf57458488afc429afe
Piet-C <piet.croockewit@gmail.com>
2026-07-08 13:26:46 +0000

    Update next.config.ts

--- piet-site/next.config.ts
+++ piet-site/next.config.ts
@@ -1,14 +1,9 @@
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "pub-08323d96cffd4fa995d320dd3deca113.r2.dev",
-      },
+      { protocol: "https", hostname: "*.supabase.co" },
+      { protocol: "https", hostname: "pub-08323d96cffd4fa995d320dd3deca113.r2.dev" },
     ],
   },
 }
